feed697d7d298a5f064546de22a8b51999e5948a
luisciaramicoli <luisgustavobonfim996@gmail.com>
2026-08-26 23:39:42 -0300

    fix: suporte para DB_PASSWORD como variavel de ambiente

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ require('dotenv').config({ path: '../.env' }); // Load env variables from projec
 const pool = mysql.createPool({
   host: process.env.DB_HOST || '127.0.0.1',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '',
+  password: process.env.DB_PASSWORD || process.env.DB_PASS || '',
   database: process.env.DB_NAME || 'cloud_atividade',
   waitForConnections: true,
   connectionLimit: 10,
